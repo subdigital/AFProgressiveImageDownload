a0c8d8b90dcde970f42e56190af07eec079f0da0
Marin Usalj <mneorr@gmail.com>
2013-06-30 02:56:01 +0200

    Fix startup nib crash
    The app crashes on start, most likely due to some file renaming.

--- Example/AFAppDelegate.m
+++ Example/AFAppDelegate.m
@@ -16,7 +16,7 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.viewController = [[ViewController alloc] initWithNibName:@"AFViewController" bundle:nil];
+    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
